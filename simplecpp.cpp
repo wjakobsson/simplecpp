5ac75f618450b4e0b175d76bee5b05d1c669ec7c
William Jakobsson <william.jakobsson@cppcheck.com>
2026-05-26 10:17:02 +0200

    remove

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -777,9 +777,6 @@ void simplecpp::TokenList::readfile(Stream &stream, const std::string &filename,
                     if (pos + 1 < currentToken.size() && currentToken[pos + 1] == '\n') {
                         currentToken.erase(pos, 2);
                         ++spliced;
-                    } else if (pos + 2 < currentToken.size() && currentToken[pos + 1] == '\r' && currentToken[pos + 2] == '\n') {
-                        currentToken.erase(pos, 3);
-                        ++spliced;
                     } else {
                         ++pos;
                     }
